3c05ebd960d5eaf38777da2e6f46065a1251713b
Evillgranny <44546756+Evillgranny@users.noreply.github.com>
2020-03-26 03:50:10 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let nArray = String(n).split('')
+    let newString = ''
+
+    if (nArray[0] === '-') {
+        nArray.splice(0, 1)
+    }
+
+    for (let i = nArray.length - 1; i >= 0; i--)
+        newString += nArray[i];
+
+
+    return Number(newString)
 }
